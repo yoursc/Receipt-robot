b6e99349ceb1aab54a90883754ad529e0cf46340
Yoursc <peiwencao@vip.qq.com>
2024-12-13 17:20:49 +0800

    ADD 添加日志

--- ClassifyFile.py
+++ ClassifyFile.py
@@ -7,8 +7,10 @@
 import os
 import shutil
 import re
+import logging
 
 # 参数配置
+logging.basicConfig(level=logging.DEBUG)
 # 工作空间路径
 CONF_WORK_SPACE = "C:\\Users\\ven\\OneDrive - 东南\\单据扫描\\作业单"
 # 编号白名单
@@ -19,7 +21,7 @@ CONF_CAR_LIST_IN = ['0000',
                     '1015',
                     '2001', '2002', '2003', '2005'
                     ]
-CONF_CAR_LIST_OUT = ['刘永杰', '李新伟']
+CONF_CAR_LIST_OUT = ['刘永杰', '李新伟', '康菊会']
 
 # 全局变量
 # 临时文件路径
@@ -34,6 +36,7 @@ def check_path():
     """
     global CONF_WORK_SPACE, WORK_TMP, ERR_FLAG
     # 如果没有定义工作目录，则将脚本文件所在目录的父目录视作工作目录
+    logging.debug('检查工作目录')
     if CONF_WORK_SPACE.isspace():
         class_file_path = os.path.realpath(__file__)
         tcp = os.path.abspath(os.path.join(
@@ -42,26 +45,26 @@ def check_path():
             ".."
         ))
         if not os.path.basename(tcp) == "作业单":
-            print(tcp)
+            logging.info(tcp)
             raise ValueError("工作目录应为作业单")
         CONF_WORK_SPACE = tcp
 
     # 检查导入目录是否存在
     WORK_TMP = os.path.join(CONF_WORK_SPACE, "待整理")
     if not os.path.exists(WORK_TMP):
-        print('输入目录不存在：', WORK_TMP)
+        logging.warning('输入目录不存在：' + WORK_TMP)
         ERR_FLAG += 1
 
     # 检查车辆子目录是否存在
     for item in CONF_CAR_LIST_IN:
         out_dir = os.path.join(CONF_WORK_SPACE, item)
         if not os.path.exists(out_dir):
-            print('输出目录不存在：', out_dir)
+            logging.warning('输出目录不存在：' + out_dir)
             ERR_FLAG += 1
     for item in CONF_CAR_LIST_OUT:
         out_dir = os.path.join(CONF_WORK_SPACE, '0000', item)
         if not os.path.exists(out_dir):
-            print('输出目录不存在：', out_dir)
+            logging.warning('输出目录不存在：' + out_dir)
             ERR_FLAG += 1
     if ERR_FLAG > 0:
         raise ValueError("有未解决的错误，程序停止运行")
@@ -79,7 +82,7 @@ def categorize():
     for item in os.scandir(WORK_TMP):
         # 检测目录文件
         if os.path.isdir(item.path):
-            print("    Warning: It is dir：" + item.name)
+            logging.warning("这个是目录：" + item.name)
             ERR_FLAG += 1
 
         # 检测文件命名是否合规
@@ -91,13 +94,13 @@ def categorize():
         # 车辆编号核查
         car_no = item.name.split("-")[1]
         if car_no not in CAR_LIST:
-            print("    Error: Not in car list: " + item.name)
+            logging.warning("车辆编号不在白名单中: " + item.name)
             ERR_FLAG += 1
 
         # 本地文件是否重复
         p = os.path.join(CONF_WORK_SPACE, car_no, item.name)
         if os.path.exists(p):
-            print("    Error: Local file is exist: " + item.name)
+            logging.error("目标路径文件已存在: " + item.name)
             ERR_FLAG += 1
 
     if ERR_FLAG > 0:
@@ -109,7 +112,7 @@ def categorize():
             continue
         if re.match(reg, item.name) is None:
             continue
-        print(item.name)
+        logging.info('移动: ' + item.name)
         car_no = item.name.split("-")[1]
         shutil.move(item.path, os.path.join(CONF_WORK_SPACE, car_no))
 
